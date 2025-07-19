13457acc6eb0b8c8c77ef51e04c8066cd1c72779
Robin Ryan <robin1ryan@hotmail.com>
2025-07-19 16:49:41 +0300

    added nice underline to navbar buttons

--- src/UI/UIComponents/NAVBAR/navbar.jsx
+++ src/UI/UIComponents/NAVBAR/navbar.jsx
@@ -1,12 +1,38 @@
 import classes from "./navbar.module.scss";
 import screen_classes from "../screenContainer/SCREENCONTAINER.module.scss";
 import ButtonWithContent from "../../../abstract/components/buttonWithContent/BUTTONWITHCONTENT.jsx";
+import { useState } from "react";
+
+const NavButton = ({ children, link }) => {
+  return (
+    <div className={classes.button}>
+      <ButtonWithContent link={link}>{children}</ButtonWithContent>
+    </div>
+  );
+};
 
 const NavbarButtons = () => {
+  const [activeButton, setActiveButton] = useState(0);
+  const [hoverButton, setHoverButton] = useState(0);
+
+  const buttons = ["Home", "About", "Contact"];
+
   return (
     <div className={classes.buttonsWrapper}>
-      <ButtonWithContent link={"/"}>Home</ButtonWithContent>
-      <ButtonWithContent link={"/Hello"}>Hello</ButtonWithContent>
+      {buttons.map((button, index) => (
+        <div
+          key={index}
+          onClick={() => setActiveButton(index)}
+          onMouseEnter={() => setHoverButton(index)}
+          onMouseLeave={() => setHoverButton(activeButton)}
+        >
+          <NavButton>{button}</NavButton>
+        </div>
+      ))}
+      <span
+        className={classes.buttonUnderline}
+        style={{ left: `${140 * hoverButton}px` }}
+      ></span>
     </div>
   );
 };
