56f0257ee575c9aab444be610c8d90cedf6b894d
Robin Ryan <robin1ryan@hotmail.com>
2025-07-24 15:43:41 +0300

    spent time with ellie and inspected/talked about ideas for the upper-section on front-page. Will continue later as well

--- src/UI/UIComponents/BODY/mainPage/mainpageImageTwo/mainPageImageTwo.jsx
+++ src/UI/UIComponents/BODY/mainPage/mainpageImageTwo/mainPageImageTwo.jsx
@@ -2,13 +2,21 @@ import classes from "./mainpageImageTwo.module.scss";
 
 import imageExampleOne from "@assets/imageOnWallPlaceholderRepresentation.png";
 import imageExampleTwo from "@assets/imageOnWallPlaceholderRepresentationTwo.png";
+
+import blueImageOne from "@assets/frontPageMainImages/mainImageOne.jpg";
+import blueImageTwo from "@assets/frontPageMainImages/mainImageTwo.jpg";
+import blueImageThree from "@assets/frontPageMainImages/mainImageThree.jpg";
+
 import { useState } from "react";
 
 const MainPageImageTwo = () => {
   return (
     <div className={classes.MainPageImageTwo}>
       <div className={classes.imagesContainer}>
-        <img className={classes.imageExample} src={imageExampleOne} alt="" />
+        <img className={classes.imageExample} src={blueImageTwo} alt="" />
+        <img className={classes.imageExample} src={blueImageOne} alt="" />
+        <img className={classes.imageExample} src={blueImageThree} alt="" />
+        {/* <img className={classes.imageExample} src={imageExampleOne} alt="" /> */}
       </div>
     </div>
   );
