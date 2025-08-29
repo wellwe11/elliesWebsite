c868082cc00020a3cc40ca7ccc676c01fb5fea9e
Robin Ryan <robin1ryan@hotmail.com>
2025-08-29 10:21:19 +0300

    updated and added styles to the filter-list. Will continue after break

--- src/UI/UIComponents/BODY/gallery/filterSideBar/filterSideBar.jsx
+++ src/UI/UIComponents/BODY/gallery/filterSideBar/filterSideBar.jsx
@@ -6,12 +6,18 @@ const FilterSideBar = ({ data, filter, setFilter, setPage }) => {
 
   const dataKeys = Object.keys(data); // all dataKeys are Object names, so dataKeys is i.e. paintings, prints etc.
 
+  console.log(filter);
+
   return (
     <div className={classes.filterSideBar}>
       <ul className={classes.filterUl}>
         {dataKeys.map((key, index) => (
           <li key={index} className={classes.filterLi}>
-            <label className={classes.filterLabel}>
+            <label
+              className={`${classes.filterLabel} ${
+                key === filter ? classes.activeFilter : ""
+              }`}
+            >
               <input
                 className={classes.filterInput}
                 type="checkbox"
@@ -20,7 +26,12 @@ const FilterSideBar = ({ data, filter, setFilter, setPage }) => {
                 onChange={() => handleFilter(key)}
                 onClick={() => setPage(1)}
               />
-              {key}
+              <div className={classes.textAndUnderline}>
+                <h3 className={classes.filterText}>{key}</h3>
+                <div className={classes.underline}>
+                  <div className={classes.underlineDot} />
+                </div>
+              </div>
             </label>
           </li>
         ))}
