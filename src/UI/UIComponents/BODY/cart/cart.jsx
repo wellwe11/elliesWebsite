735780e9f8acff13883785c7ca4b91db143427aa
Robin Ryan <robin1ryan@hotmail.com>
2025-09-16 13:42:09 +0300

    working on how to display cart

--- src/UI/UIComponents/BODY/cart/cart.jsx
+++ src/UI/UIComponents/BODY/cart/cart.jsx
@@ -4,19 +4,48 @@ import { useContext, useEffect } from "react";
 import cartContext from "../cartContext";
 import bodyNoScroll from "@functions/bodyNoScroll";
 import { useNavigate } from "react-router-dom";
-import addToCart from "@functions/addToCart";
+
+const Product = ({ product }) => {
+  // for details such as color, price etc.
+  console.log(product);
+
+  const { cart } = useContext(cartContext);
+  const flattedProducts = Object.values(cart).flat();
+};
+
+const CartProducts = ({}) => {
+  const { cart } = useContext(cartContext);
+
+  const cartEntries = Object.entries(cart);
+  console.log(cartEntries);
+
+  const cartProductsWrapper = cartEntries.map(([name, arr], index) => (
+    <div key={index}>
+      <h3>{name}</h3>
+      <Product product={arr} />
+      <h3>{arr.length}</h3>
+    </div>
+  ));
+
+  return (
+    <div className={classes.cartProductsContainer}>{cartProductsWrapper}</div>
+  );
+};
 
 const Cart = () => {
   const navigate = useNavigate();
   const { cart, setCart, totalItems, totalPrice } = useContext(cartContext);
   const { disableScroll, enableScroll } = bodyNoScroll();
 
+  const totalPriceVar = Math.floor(+totalPrice() * 10) / 10; // 19.999... === 19.9
+  const totalItemsVar = totalItems();
+
   // if objects have same id, stack them inside the same array
   // sorts items before viewing cart
 
   // add + and - button to increase/decrease amount of objects per item
 
-  console.log(cart, totalItems(), totalPrice());
+  console.log(cart, totalPriceVar, totalItemsVar);
 
   useEffect(() => {
     disableScroll();
@@ -32,7 +61,9 @@ const Cart = () => {
       <div className={classes.background} onClick={handleNavigateBack} />
       <div className={classes.cartWrapper}>
         <h1>cart</h1>
-        <h1>{cart?.length}</h1>
+        <h1>Total items: {totalItemsVar}</h1>
+        <h1>Total price: {totalPriceVar}</h1>
+        <CartProducts />
       </div>
     </div>
   );
--- src/abstract/functions/addToCart.js
+++ src/abstract/functions/addToCart.js
@@ -1,21 +1,17 @@
 const addToCart = (setter, obj) => {
   // objects id is used as key for stacking objects having the same id
   // because each product has a unique id, we need to stack them to control them further in the cart and for more easily displayed products that are in cart
-  const objId = obj.id;
-  const objType = obj._embedded.details.type;
-
-  const getterKey = `${objType} ${+objId}`;
-  console.log(getterKey);
+  const objName = obj._embedded.setTitle;
 
   setter((prev) => {
     const previousCart = { ...prev };
 
-    if (!previousCart[getterKey]) {
+    if (!previousCart[objName]) {
       // if no key inside of previousCart has objId, make one
-      previousCart[getterKey] = [];
+      previousCart[objName] = [];
     }
 
-    previousCart[getterKey] = [...previousCart[getterKey], obj];
+    previousCart[objName] = [...previousCart[objName], obj];
     return previousCart;
   });
 };
