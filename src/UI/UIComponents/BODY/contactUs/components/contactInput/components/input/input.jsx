ea31cd6d7e4e8d5233b12d3611487e25fba0e505
Robin Ryan <robin1ryan@hotmail.com>
2025-09-26 12:56:16 +0300

    i have given the contact a generic style as well as configured word-spacing that suits the website well in the index.scss document

--- src/UI/UIComponents/BODY/contactUs/components/contactInput/components/input/input.jsx
+++ src/UI/UIComponents/BODY/contactUs/components/contactInput/components/input/input.jsx
@@ -3,7 +3,7 @@ import classes from "./input.module.scss";
 const Input = ({ children = "Please add text", type = "text" }) => {
   return (
     <div className={classes.inputWrapper}>
-      <h6 className={classes.inputTitle}>{children}</h6>
+      <p className={classes.inputTitle}>{children}</p>
       <input className={classes.input} type={type} />
     </div>
   );
