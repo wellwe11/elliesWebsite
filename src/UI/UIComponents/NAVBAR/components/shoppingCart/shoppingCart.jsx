ffd173d37ff11b5e76a5f4c2c77962bb1838eec5
Robin Ryan <robin1ryan@hotmail.com>
2025-10-06 09:45:28 +0300

    chore(shoppingCart): Abstracted logic away from navbar into it's own seperate file

---
+++ src/UI/UIComponents/NAVBAR/components/shoppingCart/shoppingCart.jsx
@@ -0,0 +1,52 @@
+import classes from "./shoppingCart.module.scss";
+
+import { useNavigate } from "react-router-dom";
+import { useMemo } from "react";
+
+import ShoppingBagSVG from "@components/SVGS/shoppingBagSVG/shoppingBagSVG";
+
+const ShoppingCart = ({ cartItems }) => {
+  const navigate = useNavigate();
+
+  const totalItemsInCart = useMemo(() => cartItems(), [cartItems]);
+
+  const navigateCart = () => {
+    if (location.pathname !== "/cart") {
+      navigate("./cart", {
+        state: { backgroundLocation: location.pathname + location.search },
+      });
+    }
+  };
+
+  // Simple text related to shoppingcart. Text is static and will remain the same.
+  const shoppingBagText = (
+    <div className={classes.shoppingBagText}>
+      <p className={`${classes.cartText} ${classes.whiteText}`}>View cart</p>
+      <p className={classes.cartText}>View cart</p>
+      <p className={`${classes.cartText} ${classes.whiteText}`}>View cart</p>
+    </div>
+  );
+
+  const totalItemsInCartWrapper = totalItemsInCart > 0 && (
+    <div className={classes.totalItemsInCartWrapper}>
+      <p className={classes.text}>{totalItemsInCart}</p>
+    </div>
+  );
+
+  // svg-wrapper
+  const shoppingBagSvgWrapper = (
+    <div className={classes.shoppingBagWrapper}>
+      <ShoppingBagSVG />
+    </div>
+  );
+
+  return (
+    <div className={classes.shoppingCart} onClick={navigateCart}>
+      {shoppingBagText}
+      {shoppingBagSvgWrapper}
+      {totalItemsInCartWrapper}
+    </div>
+  );
+};
+
+export default ShoppingCart;
--- src/UI/UIComponents/NAVBAR/navbar.jsx
+++ src/UI/UIComponents/NAVBAR/navbar.jsx
@@ -3,73 +3,13 @@ import classes from "./navbar.module.scss";
 // importing screen-sizings to help navbar scale with body
 import screen_classes from "../screenContainer/SCREENCONTAINER.module.scss";
 
-import { useNavigate } from "react-router-dom";
-import { useMemo } from "react";
-
 import NavLogo from "./navLogo/navLogo.jsx";
 
 import ButtonWithContent from "@components/buttonWithContent/BUTTONWITHCONTENT.jsx";
-import ShoppingBagSVG from "@components/SVGS/shoppingBagSVG/shoppingBagSVG";
+
 import useSetActiveNavButton from "./hooks/useSetActiveNavButton.jsx";
 import NavbarButtons from "./components/navBarButtons/navBarButtons.jsx";
-
-const ShoppingCart = ({ cartItems }) => {
-  const navigate = useNavigate();
-
-  const totalItemsInCart = useMemo(() => cartItems(), [cartItems]);
-
-  const navigateCart = () => {
-    if (location.pathname !== "/cart") {
-      navigate("./cart", {
-        state: { backgroundLocation: location.pathname + location.search },
-      });
-    }
-  };
-
-  // Simple text related to shoppingcart. Text is static and will remain the same.
-  const shoppingBagText = (
-    <div className={classes.shoppingBagText}>
-      <p className={`${classes.cartText} ${classes.whiteText}`}>View cart</p>
-      <p className={classes.cartText}>View cart</p>
-      <p className={`${classes.cartText} ${classes.whiteText}`}>View cart</p>
-    </div>
-  );
-
-  const totalItemsInCartWrapper = totalItemsInCart > 0 && (
-    <div className={classes.totalItemsInCartWrapper}>
-      <p className={classes.text}>{totalItemsInCart}</p>
-    </div>
-  );
-
-  // svg-wrapper
-  const shoppingBagSvgWrapper = (
-    <div className={classes.shoppingBagWrapper}>
-      <ShoppingBagSVG />
-    </div>
-  );
-
-  return (
-    <div className={classes.shoppingCart} onClick={navigateCart}>
-      {shoppingBagText}
-      {shoppingBagSvgWrapper}
-      {totalItemsInCartWrapper}
-    </div>
-  );
-};
-
-// a wrapper for each nav-button
-const NavButton = ({ children, link }) => {
-  const navigate = useNavigate();
-
-  return (
-    // on-click is applied to wrapper to isolate logic from buttonWithContent
-    <div className={classes.button}>
-      <ButtonWithContent onClick={() => navigate(link)} fontType={"h6"}>
-        {children}
-      </ButtonWithContent>
-    </div>
-  );
-};
+import ShoppingCart from "./components/shoppingCart/shoppingCart.jsx";
 
 const Navbar = ({ cartItems }) => {
   const buttons = {
