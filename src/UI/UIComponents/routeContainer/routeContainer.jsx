0b5492df0a283763e8da62042211e8a1050844be
Robin Ryan <robin1ryan@hotmail.com>
2025-10-14 13:47:07 +0300

    chore(routeContainer): rewrote storeData; Zustand to match my useContext and it's methods

--- src/UI/UIComponents/routeContainer/routeContainer.jsx
+++ src/UI/UIComponents/routeContainer/routeContainer.jsx
@@ -1,13 +1,9 @@
 import classes from "./routeContainer.module.scss";
 
-import { Suspense, useState } from "react";
+import { Suspense } from "react";
 
 import Footer from "../FOOTER/footer.jsx";
 import Navbar from "../NAVBAR/navbar.jsx";
-
-import cartContext from "../BODY/cartContext.jsx";
-
-import getTotalInfo from "./functions/totalItems.js";
 
 import MainPagesRoutes from "./components/mainPageRoutes/mainPageRoutes.jsx";
 import BackgroundRoutes from "./components/backgroundRoutes/backgroundRoutes.jsx";
@@ -18,29 +14,31 @@ import { create } from "zustand";
 
 // is for future, will store data in here to fetch it over the page
 export const storeData = create((set) => ({
-  data: null,
-  setData: (d) => set({ data: d }),
-
   cart: {},
   setCart: (e) => set({ cart: e }),
+
+  totalItems: 0,
+  setTotalItems: (e) => set({ totalItems: Object.values(e)?.flat()?.length }),
+
+  totalPrice: 0,
+  setTotalPrice: (e) => ({
+    totalPrice: Object.values(e)
+      .flat()
+      .reduce((sum, item) => sum + item?._embedded?.details?.price, 0),
+  }),
 }));
 
 const RouteContainer = () => {
-  const [cart, setCart] = useState({}); // context for whichever product is in focus
-
-  const { totalItems, totalPrice } = getTotalInfo(cart);
-
   const { state } = useGetLocation();
 
   return (
     <div className={classes.widthContainer}>
-      <Navbar cartItems={totalItems} />
+      <Navbar />
 
       <div className={`${classes.contentWrapper} ${classes.paddingClass}`}>
-        <cartContext.Provider value={{ cart, setCart, totalItems, totalPrice }}>
-          <MainPagesRoutes />
-          {state?.backgroundLocation && <BackgroundRoutes />}
-        </cartContext.Provider>
+        <MainPagesRoutes />
+        {state?.backgroundLocation && <BackgroundRoutes />}
+
         <Footer />
       </div>
     </div>
