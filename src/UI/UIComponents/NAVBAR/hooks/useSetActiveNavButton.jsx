3738b5f2857bf0432d93e6c7dd792d3144f733f1
Robin Ryan <robin1ryan@hotmail.com>
2025-10-17 18:39:17 +0300

    chore(useSetActiveNavButton): moved setActiveNavButton function from a seperated folder into the useSetActiveNavButton so that the logic is isolated together, since it's actually not being used anywhere else. It's just easier to understand what is going on with the code being together

--- src/UI/UIComponents/NAVBAR/hooks/useSetActiveNavButton.jsx
+++ src/UI/UIComponents/NAVBAR/hooks/useSetActiveNavButton.jsx
@@ -1,6 +1,37 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import setActiveNavButton from "../functions/setActiveNavButton.js";
+
+const setActiveNavButton = (
+  buttons,
+  pathname,
+  setActiveButton,
+  setHoverButton,
+  setLoadTab
+) => {
+  // find active tab
+  // split names of tabs
+  const buttonsKeys = Object.keys(buttons);
+
+  // split current link
+  const splittedPathName = pathname.split("/");
+  console.log(splittedPathName);
+
+  // find if any buttonKeys exist inside of url
+  buttonsKeys.forEach((btn, index) => {
+    if (
+      splittedPathName.includes(btn) ||
+      (btn === "home" &&
+        splittedPathName.includes("") &&
+        !pathname.includes("uniqueImage"))
+    ) {
+      // if yes, change index to that
+      setActiveButton(index);
+      setHoverButton(index);
+    }
+  });
+
+  setLoadTab(true);
+};
 
 const useSetActiveNavButton = (buttons) => {
   const { pathname } = useLocation();
