21b3847091cc4668ba178fa6d6deca49a9aad2eb
Robin Ryan <robin1ryan@hotmail.com>
2025-10-18 16:19:55 +0300

    chore(products): product now has a explore button which navigates to products page

--- src/UI/UIComponents/PAGES/home/componentsPhoneSize/products/products.jsx
+++ src/UI/UIComponents/PAGES/home/componentsPhoneSize/products/products.jsx
@@ -1,7 +1,11 @@
 import classes from "./products.module.scss";
 import { capitalizeFirstLetter } from "@functions/firstLetterCapital.js";
 
+import QuickViewButton from "@fullyComponents/quickView/quickViewButton/quickViewButton.jsx";
+import { useNavigate } from "react-router-dom";
+
 const Products = ({ data }) => {
+  const navigate = useNavigate();
   const dataEntries = Object.entries(data);
   const regex = /data/i;
 
@@ -11,22 +15,33 @@ const Products = ({ data }) => {
     return [fixedTitle, obj];
   });
 
+  const handleNavigate = (category, id) => {
+    navigate(`uniqueImage?category=${category}&id=${id}`);
+  };
+
   return (
-    <div className={classes.products}>
+    <div className={classes.categories}>
       {fixedTitles.map(([entry, obj], index) => (
         <div key={index} className={classes.products}>
           <div className={classes.categoryTitleWrapper}>
             <h3 className={classes.title}>{entry}</h3>
           </div>
 
-          <div>
+          <div className={classes.products}>
             {obj.map((obj, index) => {
               const name = obj._embedded.setTitle;
               const image = obj.image;
+              const id = obj.id;
+              const category = obj._embedded.details.type;
+
               return (
-                <div key={index}>
-                  <h5>{name}</h5>
-                  <img src={image} alt="" />
+                <div key={index} className={classes.product}>
+                  <h5 className={classes.title}>{name}</h5>
+                  <img className={classes.img} src={image} alt="" />
+                  <QuickViewButton
+                    text={"Explore"}
+                    onClick={() => handleNavigate(category, id)}
+                  />
                 </div>
               );
             })}
