e1075b73e4ce67bccdd0a15973f5eb7f94e93058
Robin Ryan <robin1ryan@hotmail.com>
2025-10-24 10:49:55 +0300

    chore(mainPageTopPresentation): animating

--- src/UI/UIComponents/PAGES/home/components/mainPageTopPresentation/mainPageTopPresentation.jsx
+++ src/UI/UIComponents/PAGES/home/components/mainPageTopPresentation/mainPageTopPresentation.jsx
@@ -1,18 +1,14 @@
 import classes from "./mainPageTopPresentation.module.scss";
 
 const MainPageTopPresentation = ({ mainImage }) => {
-  // a small animation which is displayed once you load the front-page
-
-  // a background-image which is displayed on top of the front-page
-  const mainPageImage = (
-    <div className={classes.mainImageWrapper}>
-      <img className={classes.mainImage} src={mainImage} alt={""} />
-    </div>
-  );
-
   return (
     <div className={classes.MainPageTopPresentation}>
-      <div className={classes.imageWrapper}>{mainPageImage}</div>
+      <div className={classes.whiteBackground} />
+      <div className={classes.imageWrapper}>
+        <div className={classes.mainImageWrapper}>
+          <img className={classes.mainImage} src={mainImage} alt={""} />
+        </div>
+      </div>
     </div>
   );
 };
