21e064737aff3d8cef187c2983dd7a66f6d4db1b
Robin Ryan <robin1ryan@hotmail.com>
2025-10-28 17:41:47 +0200

    chore(gallery/hooks/useProductgsLogic.jsx): changed to use useMemo instead of useEffect to improve updates between each render

--- src/UI/UIComponents/PAGES/gallery/hooks/useProductsLogic.jsx
+++ src/UI/UIComponents/PAGES/gallery/hooks/useProductsLogic.jsx
@@ -1,24 +1,20 @@
-import { useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 
 import handleDisplayedProducts from "../functions/handleDisplayedProducts.js";
 
 // slices products based on what page user is on
 const useProductsLogic = (page, filteredData) => {
   const [isLoading, setIsLoading] = useState(false);
-  const [updatedData, setUpdatedData] = useState(() =>
-    handleDisplayedProducts(page, filteredData, 12)
-  ); // initial data
 
-  const slicedProducs = handleDisplayedProducts(page, filteredData, 12); // runs each render to slice products based on page
-
-  useEffect(() => {
+  const updatedData = useMemo(() => {
+    const slicedProducs = handleDisplayedProducts(page, filteredData, 12); // runs each render to slice products based on page
     setIsLoading(true);
 
-    if (page) {
-      setUpdatedData(slicedProducs);
+    if (slicedProducs) {
       window.scrollTo({ top: 0 });
+      setIsLoading(false);
+      return slicedProducs;
     }
-    setIsLoading(false);
   }, [page, filteredData]);
 
   return { updatedData, isLoading };
