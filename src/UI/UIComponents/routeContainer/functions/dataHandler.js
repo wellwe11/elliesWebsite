6903f8a127049a2919ab51c5a530d059ef97178a
Robin Ryan <robin1ryan@hotmail.com>
2025-11-19 14:15:08 +0200

    fix(dataHandler): user can now select several filters

--- src/UI/UIComponents/routeContainer/functions/dataHandler.js
+++ src/UI/UIComponents/routeContainer/functions/dataHandler.js
@@ -11,7 +11,10 @@ const dataHandler = (data, categories) => {
       const details = obj._embedded.details;
       const objCategory = details.type;
 
-      const detailsValues = Object.values(details);
+      const { type, ...detailsWithoutType } = details;
+
+      const detailsValues = Object.values(detailsWithoutType);
+      console.log(detailsWithoutType);
 
       /**
        * User clicks one category type (paintings/prints)
@@ -24,23 +27,29 @@ const dataHandler = (data, categories) => {
 
       const productMustMatch = categories.every((e) => ObjKeys.includes(e)); // if only category (paintings/prints) is active
       if (productMustMatch) {
+        console.log(1);
         // Only category is active (prints/paintings)
         return categories.includes(objCategory);
       } else {
+        console.log(2);
         // If filter is active (set, single, etc.)
 
         // if all objKeys are active (paintings, prints)
         if (ObjKeys.every((e) => categories.includes(e))) {
+          console.log(2.1);
+
           return (
             categories.some((e) => detailsValues.includes(e)) &&
             categories.includes(objCategory)
           );
           // if one objKey filter (paintings, prints) is active
         } else {
-          return (
-            categories.every((e) => detailsValues.includes(e)) &&
-            categories.includes(objCategory)
-          );
+          console.log(2.2);
+
+          return !ObjKeys.some((e) => categories.includes(e))
+            ? categories.some((e) => detailsValues.includes(e))
+            : categories.some((e) => detailsValues.includes(e)) &&
+                categories.includes(objCategory);
         }
       }
     });
