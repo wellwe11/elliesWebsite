86c8c36ba614192cba44969efa3c1254ef556870
Robin Ryan <robin1ryan@hotmail.com>
2025-11-25 17:15:23 +0200

    making it more dynamic and abstract. Still have to fix a bit of code

--- src/UI/UIComponents/routeContainer/functions/dataHandler.js
+++ src/UI/UIComponents/routeContainer/functions/dataHandler.js
@@ -1,52 +1,106 @@
-const fixObjects = (data) => {
+// fixObjects rebuilds data into more readable and flexible objects.
+// Data that is fetched contains both parent-objects as well as child-objects which needs to be flatted out for user-filtering
+
+const CopyData = (data) => {
   const flattedDataSets = Object.values(data).flat();
+  return JSON.parse(JSON.stringify(flattedDataSets));
+};
 
-  const collectionSets = flattedDataSets.filter((obj) => obj.set === "set");
+const removeProperty = (obj, propertyToRemove) => {
+  const { [propertyToRemove]: discarded, ...rest } = obj;
 
+  return rest;
+};
+
+const extractKeys = (obj, ...keysToExtract) => {
+  const extractedDetails = {};
+
+  keysToExtract.forEach((key) => {
+    if (obj.hasOwnProperty(key)) {
+      extractedDetails[key] = obj[key];
+    }
+  });
+
+  return extractedDetails;
+};
+
+const extractDetailsFromChildren = (obj) => {
+  const details = obj.collection[0].details;
+  const image = obj.collection[0].image;
+
+  const extractedKeys = extractKeys(
+    details,
+    "price",
+    "colors",
+    "height",
+    "width"
+  );
+
+  const newObj = removeProperty(obj, "collection");
+
+  return {
+    ...newObj,
+    ...extractedKeys,
+    image,
+  };
+};
+
+const extractDetailsFromParent = (obj) => {
+  const extractedKeys = extractKeys(obj, "setTitle", "setDescription", "type");
+
+  return obj.collection.map((e, index) => {
+    const productId = `${obj.id}.${index}`;
+
+    return {
+      ...e,
+      ...extractedKeys,
+      id: productId,
+      set: "single",
+    };
+  });
+};
+
+const extractFromCollection = (data) => {
   // individual nested objects
-  const flattedIndividualSets = flattedDataSets
+  return data
     .map((obj) => {
-      const { setTitle, setDescription, type } = obj;
+      const isCollection = obj.collection.length > 1;
 
-      obj.set = obj.collection.length > 1 ? "set" : "single";
-      obj.image = obj.setImages[0]; // select a default image
-      obj.price = 0; // add a total price
+      const totalPrice = obj.collection.reduce(
+        (acc, item) => acc + item.details.price,
+        0
+      );
 
-      // if collection is a single item
-      if (obj.collection.length === 1) {
-        const { price, colors, height, width } = obj.collection[0].details,
-          image = obj.collection[0].image;
+      const updatedObj = {
+        ...obj,
+        set: isCollection ? "set" : "single",
+        image: obj.setImages[0],
+        price: totalPrice,
+      };
 
-        const { collection, ...newObj } = obj; // remove set as it is no longer needed
-
-        return {
-          ...newObj,
-          price,
-          image,
-          colors,
-          height,
-          width,
-        };
+      // if obeject is a single item. I.e. item of a collection
+      if (!isCollection) {
+        // extract child-details so parent can display matching information
+        return extractDetailsFromChildren(updatedObj);
       }
 
-      // if collection is a set
-      if (obj.collection.length > 1) {
-        return obj.collection.map((e, index) => {
-          const productId = +`${obj.id}.${index}`;
-          obj.price += e.details.price; // add to sets total price
-
-          return {
-            ...e,
-            setTitle,
-            setDescription,
-            type,
-            id: productId,
-            set: "single",
-          };
-        });
+      // if obj is a collection
+      if (isCollection) {
+        // apply details from parent-object so children can have matching set-information
+        return extractDetailsFromParent(updatedObj);
       }
     })
     .flat();
+};
+
+const fixObjects = (data) => {
+  const deepCopyDataSets = CopyData(data);
+  const flattedIndividualSets = extractFromCollection(deepCopyDataSets);
+
+  // for tomorrow: need to handle sets:
+  // All objects should be displayable at same time, meaning, they all need individual ids (applied before filtering/using aboive functions)
+
+  const collectionSets = deepCopyDataSets.filter((obj) => obj.set === "set");
 
   return { collectionSets, flattedIndividualSets };
 };
@@ -69,18 +123,23 @@ const dataHandler = (data, categories) => {
 
   let isFilterActive = arr.flat().length > 0;
 
+  // single filter used
   if (categories.includes("single") && !categories.includes("set")) {
     const { flattedIndividualSets } = fixObjects(isFilterActive ? arr : data);
+
     return flattedIndividualSets.flat();
   }
 
+  // set filter used
   if (categories.includes("set") && !categories.includes("single")) {
     const { collectionSets } = fixObjects(isFilterActive ? arr : data);
-
     return collectionSets.flat();
   }
 
-  const { collectionSets, flattedIndividualSets } = fixObjects(arr);
+  const { collectionSets, flattedIndividualSets } = fixObjects(
+    isFilterActive ? arr : data
+  );
+
   return [collectionSets, flattedIndividualSets].flat();
 };
 
